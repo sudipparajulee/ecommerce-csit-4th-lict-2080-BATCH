912272a317702db6455c8508aac728bee8f086b8
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-06-09 10:57:19 +0545

    Product Migration

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,7 +4,7 @@
     <div class="grid grid-cols-3 gap-4">
         <div class="bg-blue-100 p-4 rounded-lg shadow">
             <h2 class="font-bold text-xl">Total Categories</h2>
-            <p class="text-3xl font-bold">44</p>
+            <p class="text-3xl font-bold">{{$totalcategories}}</p>
         </div>
         <div class="bg-red-100 p-4 rounded-lg shadow">
             <h2 class="font-bold text-xl">Total Products</h2>
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,14 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PagesController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[PagesController::class, 'index']);
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [DashboardController::class, 'dashboard'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
 Route::get('/categories/create', [CategoryController::class, 'create'])->name('categories.create');
