bc63fad776f8e676277f3b97b5d461d48419f5a7
Sudip Parajuli <sudip.parajulee@gmail.com>
2025-06-11 10:57:19 +0545

    updats

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -15,12 +16,30 @@ class ProductController extends Controller
 
     public function create()
     {
-        return view('products.create');
+        $categories = Category::orderBy('order', 'asc')->get();
+        return view('products.create', compact('categories'));
     }
 
     public function store(Request $request)
     {
+        $data = $request->validate([
+            'name' => 'required',
+            'price' => 'required|numeric',
+            'discounted_price' => 'nullable|numeric',
+            'description' => 'nullable|string',
+            'category_id' => 'required',
+            'stock' => 'required|integer',
+            'photopath' => 'required|image'
+        ]);
 
+        //handle file upload
+        $file = $request->file('photopath');
+        $filename = time() . '.' . $file->getClientOriginalExtension();
+        $file->move(public_path('images/products/'), $filename);
+        $data['photopath'] = $filename;
+
+        Product::create($data);
+        return redirect()->route('products.index')->with('success', 'Product created successfully.');
     }
 
     public function edit($id)
--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -1,16 +1,44 @@
 @extends('layouts.app')
 @section('title', 'Create Product')
 @section('content')
-<form action="{{route('products.store')}}" method="POST">
+<form action="{{route('products.store')}}" method="POST" enctype="multipart/form-data">
         @csrf
         <select name="category_id" id="" class="border border-gray-300 p-2 rounded w-full mb-4">
-            <option value="">Electronics</option>
-            <option value="">Grocerries</option>
+            @foreach($categories as $category)
+            <option value="{{$category->id}}">{{$category->name}}</option>
+            @endforeach
         </select>
         <input type="text" name="name" placeholder="Product Name" class="border border-gray-300 p-2 rounded w-full mb-4" value="{{ old('name') }}">
         @error('name')
             <div class="text-red-500 mb-2 -mt-3">{{ $message }}</div>
         @enderror
+
+        <input type="text" name="price" placeholder="Price" class="border border-gray-300 p-2 rounded w-full mb-4" value="{{ old('price') }}">
+        @error('price')
+            <div class="text-red-500 mb-2 -mt-3">{{ $message }}</div>
+        @enderror
+
+        <input type="text" name="discounted_price" placeholder="Discounted Price" class="border border-gray-300 p-2 rounded w-full mb-4" value="{{ old('discounted_price') }}">
+        @error('discounted_price')
+            <div class="text-red-500 mb-2 -mt-3">{{ $message }}</div>
+        @enderror
+
+        <textarea name="description" placeholder="Description" class="border border-gray-300 p-2 rounded w-full mb-4">{{ old('description') }}</textarea>
+        @error('description')
+            <div class="text-red-500 mb-2 -mt-3">{{ $message }}</div>
+        @enderror
+
+        <input type="text" name="stock" placeholder="Stock" class="border border-gray-300 p-2 rounded w-full mb-4" value="{{ old('stock') }}">
+        @error('stock')
+            <div class="text-red-500 mb-2 -mt-3">{{ $message }}</div>
+        @enderror
+
+        <input type="file" name="photopath" class="border border-gray-300 p-2 rounded w-full mb-4">
+        @error('photopath')
+            <div class="text-red-500 mb-2 -mt-3">{{ $message }}</div>
+        @enderror
+
+
         <div class="flex justify-center">
             <button type="submit" class="bg-blue-500 px-4 py-2 rounded text-white">Create Product</button>
             <a href="{{route('products.index')}}" class="bg-gray-300 px-4 py-2 rounded text-black ml-2">Cancel</a>
